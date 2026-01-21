162539451042193e995e5343d9d472cd018de28e
zidanekarim <zkarim7676@gmail.com>
2026-01-21 15:11:04 -0500

    yo

--- programming1/main.cpp
+++ programming1/main.cpp
@@ -3,27 +3,41 @@
 #include "foo.h"
 #include "foobar.h"
 #include <fstream>
+#include <sstream>
+
 #using namespace std
+
+
 int main() {
+    vector<Foobar>* Foobars;
     cout << "Enter your input filename:\n";
     string input;
     cin >> input;
+
+    processInput(input, Foobars);
 
     cout << "Enter your output filename:\n";
     string output;
     cin >> output;
 
+    
+
+
+    return 0;
+}
+
+int processInput(string input, vector<Foobar>* foobars) {
     ifstream infile; // sourced from https://stackoverflow.com/questions/13035674/how-to-read-a-file-line-by-line-or-a-whole-text-file-at-once
     infile.open(input);
     string line;
 
     int count;
-    vector<Foobar> Foobars;
-
+    
     while (getline(infile, line)) {
-        
+        stringstream current(line); 
+        std::string word;
+        while (current >> word) {
+            switch word 
+        }
     }
-
-
-    return 0;
 }
